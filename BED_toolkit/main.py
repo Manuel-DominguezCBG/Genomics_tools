64b6bafda7452a21a6b65060c4b997f743e23758
Monkiky <monkiky@Monkikys-MacBook-Pro.local>
2021-10-29 14:15:53 +0100

    Some documemntation

--- BED_toolkit/main.py
+++ BED_toolkit/main.py
@@ -6,19 +6,18 @@ from Class.read_bed_file import Read_file
 # Take the input file
 user_input = sys.argv[1]
 
-# Take
+# Run functions with input data
 bed_data = Read_file(user_input)
 data_loaded = bed_data.load_data()
 print(data_loaded)
 
 bed_data = Bed_tools(data_loaded)
 
+# Take user´s query
 user_input2 = sys.argv[2]
 chro = user_input2.split(":")[0]
 position = user_input2.split(":")[1]
 
-
-# print(chro)
-# print(position)
+# See if the position is covered by the bed file 
 print(bed_data.targe_region( data_loaded, chro, position))
 
